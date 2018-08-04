51ff0c30a460037e58d302566fbfa2491a5c64b0
AdhithyanSridhar <adhithyan90@gmail.com>
2018-08-04 11:34:23 +0530

    java doc generation

--- src/main/java/com/sample/javadoc/JavadocApplication.java
+++ src/main/java/com/sample/javadoc/JavadocApplication.java
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavadocApplication {
 
+	/*
+	 * https://www.mkyong.com/maven/generate-javadoc-jar-for-maven-based-project/
+	 * 
+	 * Do a mvn install to generate java doc for the project. Build should be
+	 * success. go to target > apidocs or check if a jar file associated with the
+	 * javadoc got generated.
+	 * 
+	 * <maven.test.skip>true</maven.test.skip>
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(JavadocApplication.class, args);
 	}
